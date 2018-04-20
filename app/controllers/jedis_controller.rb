78e9b49d47b2635afafe213a291f3af708f3df3b
funemployeddad <funemployeddad@gmail.com>
2018-04-19 19:18:21 -0700

    jedi model and controller done

--- app/controllers/jedis_controller.rb
+++ app/controllers/jedis_controller.rb
@@ -1,2 +1,31 @@
 class JedisController < ApplicationController
+  skip_before_action :verify_jedi_is_authenticated, only: [:new,:create]
+  def new
+    @jedi = Jedi.new
+  end
+
+  def create
+    if (jedi = Jedi.create jedi_params)
+      session[:jedi_id] = jedi.id
+      redirect_to jedi_path(jedi)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @jedi = Jedi.find_by(id:params[:id])
+  end
+
+  private
+  def jedi_params
+    params.require(:jedi).permit(
+      :name,
+      :strength,
+      :admin,
+      :password,
+      :hp
+      )
+    end
+
 end
--- app/models/jedi.rb
+++ app/models/jedi.rb
@@ -1,2 +1,11 @@
 class Jedi < ApplicationRecord
+  has_secure_password
+  has_many :fights
+  has_many :enemies, through: => :fights
+
+  def health
+    unless admin
+      hp > 0 ? 'alive' : 'dead'
+    end
+  end
 end
